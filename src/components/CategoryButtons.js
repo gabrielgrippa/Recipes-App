0d3276238b611e7a9fe660cfdd9bef1a9ddbe81f
Davíd Roggér <davidrogger.a@gmail.com>
2022-04-27 17:51:17 -0300

    implementada logica para renderizar botoes apartir da info do store

--- src/components/CategoryButtons.js
+++ src/components/CategoryButtons.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-function CategoryButtons() {
+function CategoryButtons({ selectedItem, meals, drinks }) {
+  const renderButtons = (categories) => (
+    categories.map((category) => (
+      <button
+        type="button"
+        key={ category }
+        data-testid={ `${category}-category-filter` }
+      >
+        {category}
+      </button>
+    ))
+  );
+
+  const currentPage = selectedItem === '/foods' ? meals : drinks;
+
   return (
     <div>
       <button type="button">All</button>
+      {renderButtons(currentPage)}
     </div>
   );
 }
+
+CategoryButtons.propTypes = {
+  selectedItem: PropTypes.string,
+  meals: PropTypes.arrayOf(PropTypes.string),
+  drinks: PropTypes.arrayOf(PropTypes.string),
+}.isRequired;
 
 const mapStateToProps = (state) => ({
   meals: state.categoryButtonsReducer.meals,
