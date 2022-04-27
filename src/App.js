bc81b3782d60086f641f27f4ad6ed7e41fdeb86f
Davíd Roggér <davidrogger.a@gmail.com>
2022-04-27 15:11:58 -0300

    importando componentes de foods, drinks e login para o app

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Switch, Route } from 'react-router-dom';
 
+import Foods from './pages/Foods';
+import Drinks from './pages/Drinks';
+import Login from './pages/Login';
+
 function App() {
   return (
+
     <Switch>
-      <Route exact to="/foods/:id" component={ Foods } />
-      <Route exact to="/foods" component={ Foods } />
-      <Route exact to="/drinks/:id" component={ Drinks } />
-      <Route exact to="/drinks" component={ Drinks } />
-      <Route exact to="/" component={ Login } />
+      <Route exact path="/foods/:id" component={ Foods } />
+      <Route exact path="/foods" component={ Foods } />
+      <Route exact path="/drinks/:id" component={ Drinks } />
+      <Route exact path="/drinks" component={ Drinks } />
+      <Route exact path="/" component={ Login } />
     </Switch>
+
   );
 }
 
