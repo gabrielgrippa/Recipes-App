36fe6bc5635c74761d4afb2700f74dd7b397aa7e
Davíd Roggér <davidrogger.a@gmail.com>
2022-04-27 15:16:07 -0300

    criando reducer para botoes de categoria

--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import CategoryButtons from '../components/CategoryButtons';
 import Header from '../components/Header';
 
 function Foods() {
   return (
     <div>
       <Header />
+      <CategoryButtons />
     </div>
   );
 }
---
+++ src/redux/reducers/categoryButtonsReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  meals: [],
+  drinks: [],
+};
+
+const categoryButtonsReducer = (state = initialState, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default categoryButtonsReducer;
