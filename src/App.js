8afa8dd5029fda05380112bf4971df32a880e94e
Davíd Roggér <davidrogger.a@gmail.com>
2022-04-27 17:54:32 -0300

    implementado solicitacao para api temporaria ao app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Switch, Route } from 'react-router-dom';
+
+import { connect } from 'react-redux';
+
+import fetchCategories from './redux/actions/getCategories';
 
 import Foods from './pages/Foods';
 import Drinks from './pages/Drinks';
 import Login from './pages/Login';
 
-function App() {
-  return (
+function App({ getCategories }) {
+  // Temporario até a existencia do login, ação deve acontecer no login.
+  useEffect(() => {
+    getCategories();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
+  return (
     <Switch>
       <Route exact path="/foods/:id" component={ Foods } />
       <Route exact path="/foods" component={ Foods } />
@@ -16,8 +26,15 @@ function App() {
       <Route exact path="/drinks" component={ Drinks } />
       <Route exact path="/" component={ Login } />
     </Switch>
-
   );
 }
 
-export default App;
+App.propTypes = {
+  getCategories: PropTypes.func,
+}.isRequired;
+
+const mapDispatchToProps = (dispatch) => ({
+  getCategories: () => dispatch(fetchCategories()),
+});
+
+export default connect(null, mapDispatchToProps)(App);
