03521c4bc64c6ba88e9d04b5130effdd4f248456
Vinícius <hello@marvig.dev>
2022-04-27 20:37:26 -0300

    setup profile store field

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import showcaseReducer from './showcaseReducer';
+import profileReducer from './profileReducer';
 
 const rootReducer = combineReducers({
   showcaseReducer,
+  profileReducer,
 });
 
 export default rootReducer;
