a82c355905b9993232405d398fca58f7b297d9c0
Davíd Roggér <davidrogger.a@gmail.com>
2022-04-27 15:48:49 -0300

    adicionado reducer para categorybuttons

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import showcaseReducer from './showcaseReducer';
+import categoryButtonsReducer from './categoryButtonsReducer';
 
 const rootReducer = combineReducers({
   showcaseReducer,
+  categoryButtonsReducer,
 });
 
 export default rootReducer;
