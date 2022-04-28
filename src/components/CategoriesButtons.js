2234cebdc2c6c84ad1a80b4d6a1bda9abecef601
Davíd Roggér <davidrogger.a@gmail.com>
2022-04-28 09:04:29 -0300

    renomeando category para categories e refatorando para uso de useSelector

---
+++ src/components/CategoriesButtons.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+
+function CategoryButtons({ selectedItem }) {
+  const meals = useSelector((state) => state.categoryButtonsReducer.meals);
+  const drinks = useSelector((state) => state.categoryButtonsReducer.drinks);
+
+  const categoryFilter = (category) => {
+    console.log(category);
+  };
+
+  const renderButtons = (categories) => (
+    <div>
+      <Button
+        data-testid="All-category-filter"
+        onClick={ () => categoryFilter('all') }
+      >
+        All
+      </Button>
+      {categories.map((category) => (
+        <Button
+          key={ category }
+          data-testid={ `${category}-category-filter` }
+          onClick={ () => categoryFilter(category) }
+        >
+          {category}
+        </Button>
+      ))}
+    </div>
+  );
+
+  const currentPage = selectedItem === '/foods' ? meals : drinks;
+
+  return renderButtons(currentPage);
+}
+
+export default CategoryButtons;
