2f9f6dd7fe5da9b35cd495e5d33840a2d396a548
Davíd Roggér <davidrogger.a@gmail.com>
2022-04-29 09:29:49 -0300

    funcionalidade de filtro ao clicar na categoria

--- src/components/CategoriesButtons.js
+++ src/components/CategoriesButtons.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
-import { CATEGORY_SEARCH } from '../redux/actions';
+import { CATEGORY_SEARCH, NAME_SEARCH } from '../redux/actions';
 import { searchAction } from '../redux/actions/showcaseActions';
+import { filterCategory } from '../redux/actions/categoriesActions';
 
 function CategoryButtons({ selectedItem }) {
   const meals = useSelector((state) => state.categoriesButtonsReducer.meals);
@@ -11,17 +12,26 @@ function CategoryButtons({ selectedItem }) {
     (state) => state.categoriesButtonsReducer.categoryFilter,
   );
   const dispatch = useDispatch();
-  console.log(categoryFilter);
 
   // Atenção: Caso a categoria retorne apenas um resultado, NÃO deve ser feito o redirecionamento para a página de detalhes.
 
+  const alreadyFiltered = (filter) => {
+    const query = filter === 'all' || filter === categoryFilter ? '' : filter;
+    const searchType = filter === 'all' || filter === categoryFilter
+      ? NAME_SEARCH : CATEGORY_SEARCH;
+    return [query, searchType];
+  };
+
   const filteringByCategory = (category) => {
     const pathname = window.location.pathname.split('/')[1];
+    const setCategory = category === categoryFilter ? 'all' : category;
 
+    dispatch(filterCategory(setCategory));
+    const [query, searchType] = alreadyFiltered(category);
     dispatch(searchAction({
       api: pathname,
-      searchType: CATEGORY_SEARCH,
-      query: category,
+      searchType,
+      query,
     }));
   };
 
