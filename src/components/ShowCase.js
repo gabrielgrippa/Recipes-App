a145dd7ca936dd75ddf26b100da578933a9ee877
Davíd Roggér <davidrogger.a@gmail.com>
2022-04-29 08:38:16 -0300

    simplificando o render de showcase

--- src/components/ShowCase.js
+++ src/components/ShowCase.js
@@ -1,34 +1,28 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { Container } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 import RecipeCard from './RecipeCard';
 
 function ShowCase() {
   const currentRecipes = useSelector((state) => state.showcaseReducer.results);
+  const pathname = window.location.pathname.split('/')[1];
+  const keyData = pathname === 'foods' ? 'Meal' : 'Drink';
+  const keyImg = `str${keyData}Thumb`;
+  const keyName = `str${keyData}`;
+  const keyId = `id${keyData}`;
 
-  const renderCards = () => {
-    const pathname = window.location.pathname.split('/')[1];
-    const keyData = pathname === 'foods' ? 'Meal' : 'Drink';
-
-    const keyImg = `str${keyData}Thumb`;
-    const keyName = `str${keyData}`;
-    const keyId = `id${keyData}`;
-
-    return (
-      <Container>
-        {currentRecipes.map((recipe, index) => (
-          <RecipeCard
-            key={ recipe[keyId] }
-            index={ index }
-            keyImg={ recipe[keyImg] }
-            keyName={ recipe[keyName] }
-          />
-        ))}
-      </Container>
-    );
-  };
-
-  return renderCards();
+  return (
+    <Container>
+      {currentRecipes.map((recipe, index) => (
+        <RecipeCard
+          key={ recipe[keyId] }
+          index={ index }
+          keyImg={ recipe[keyImg] }
+          keyName={ recipe[keyName] }
+        />
+      ))}
+    </Container>
+  );
 }
 
 export default ShowCase;
