c4a9a78b4ec5434afd37d3d2dc4a2b5c2826682e
Davíd Roggér <davidrogger.a@gmail.com>
2022-05-02 18:27:51 -0300

    implementada pagina de explore recipe

--- src/pages/ExploreRecipes.js
+++ src/pages/ExploreRecipes.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import ExploreButtons from '../components/ExploreButtons';
+
+import Header from '../components/Header';
+import { PATH_FOODS } from '../redux/actions';
+
+const foodsButtons = ['By Ingredient', 'By Nationality', 'Surprise me!'];
 
 function ExploreRecipes() {
+  const pathname = window.location.pathname.split('/')[2];
+  const currentTitle = pathname === PATH_FOODS ? 'Foods' : 'Drinks';
   return (
     <div>
-      Explore Recipes
+      <Header title={ `Explore ${currentTitle}` } enableSearch={ false } />
+      <ExploreButtons exploreButtons={ foodsButtons } />
     </div>
   );
 }
