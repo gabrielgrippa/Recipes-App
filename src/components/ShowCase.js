7003a62263933ceaa4649a1dd515c5f046b369fd
Davíd Roggér <davidrogger.a@gmail.com>
2022-05-02 17:20:15 -0300

    implementado redirecionamento caso a busca retorne apenas uma receita

--- src/components/ShowCase.js
+++ src/components/ShowCase.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 import RecipeCard from './RecipeCard';
 
 function ShowCase() {
   const currentRecipes = useSelector((state) => state.showcaseReducer.results);
+  const searchBar = useSelector((state) => state.showcaseReducer.searchBar);
   const pathname = window.location.pathname.split('/')[1];
   const keyData = pathname === 'foods' ? 'Meal' : 'Drink';
   const keyImg = `str${keyData}Thumb`;
   const keyName = `str${keyData}`;
   const keyId = `id${keyData}`;
 
+  if (currentRecipes.length === 1 && searchBar) {
+    return <Redirect to={ `/${pathname}/${currentRecipes[0][keyId]}` } />;
+  }
   return (
     <Container className="d-flex flex-wrap justify-content-center">
       {currentRecipes.map((recipe, index) => (
