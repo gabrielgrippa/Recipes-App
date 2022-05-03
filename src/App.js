779675d444bf13b41aaea85e41668fd2954fc58d
Davíd Roggér <davidrogger.a@gmail.com>
2022-05-03 09:23:04 -0300

    refatorando forma que é apontado os caminhos das paginas, para um padrao que nao precisa de quebra de linha nas tags de route

--- src/App.js
+++ src/App.js
@@ -7,15 +7,27 @@ import Recipes from './pages/Recipes';
 import RecipesDetails from './pages/RecipesDetails';
 import Explore from './pages/Explore';
 import ExploreRecipes from './pages/ExploreRecipes';
+import ExploreIngredients from './pages/ExploreIngredients';
+import ExploreNationalities from './pages/ExploreNationalities';
+
+const exploreIngredientsPath = '/explore/:recipes/ingredients';
+const exploreNationalitiesPath = '/explore/foods/nationalities';
+const exploreRecipesPath = '/explore/:recipes';
+const explorePath = '/explore';
+const recipesIdPath = '/:recipes/:id';
+const recipesPath = '/:recipes';
+const loginPath = '/';
 
 function App() {
   return (
     <Switch>
-      <Route exact path="/explore/:recipes" component={ ExploreRecipes } />
-      <Route exact path="/explore" component={ Explore } />
-      <Route exact path="/:recipes/:id" component={ RecipesDetails } />
-      <Route exact path="/:recipes" component={ Recipes } />
-      <Route exact path="/" component={ Login } />
+      <Route exact path={ exploreNationalitiesPath } component={ ExploreNationalities } />
+      <Route exact path={ exploreIngredientsPath } component={ ExploreIngredients } />
+      <Route exact path={ exploreRecipesPath } component={ ExploreRecipes } />
+      <Route exact path={ explorePath } component={ Explore } />
+      <Route exact path={ recipesIdPath } component={ RecipesDetails } />
+      <Route exact path={ recipesPath } component={ Recipes } />
+      <Route exact path={ loginPath } component={ Login } />
     </Switch>
   );
 }
