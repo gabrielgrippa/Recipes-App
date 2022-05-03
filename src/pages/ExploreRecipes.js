c0119208928fdce5fc74257f434011342c190c0c
Davíd Roggér <davidrogger.a@gmail.com>
2022-05-03 09:22:08 -0300

    logica experiemental para nao renderizar um dos botoes na pagina explore drinks

--- src/pages/ExploreRecipes.js
+++ src/pages/ExploreRecipes.js
@@ -1,19 +1,36 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import ExploreButtons from '../components/ExploreButtons';
 import Footer from '../components/Footer';
 
 import Header from '../components/Header';
 import { PATH_FOODS } from '../redux/actions';
 
-const foodsButtons = ['By Ingredient', 'By Nationality', 'Surprise me!'];
+const allButtons = ['Ingredient', 'Nationality', 'Surprise'];
 
 function ExploreRecipes() {
   const pathname = window.location.pathname.split('/')[2];
-  const currentTitle = pathname === PATH_FOODS ? 'Foods' : 'Drinks';
+  const currentURL = pathname === PATH_FOODS;
+  const currentTitle = currentURL ? 'Foods' : 'Drinks';
+  const renderButtons = currentURL
+    ? allButtons : [...allButtons.splice(0, 1), ...allButtons.splice(1, 2)];
+
+  const history = useHistory();
+
+  const handlerClick = (path) => {
+    const explorePath = path === 'ingredient' ? 'ingredients' : 'nationalities';
+    if (path === 'surprise') {
+      console.log('solicitacao random para api redicionando para tela inicial');
+      history.push(`/${pathname}`);
+    } else {
+      history.push(`/explore/${pathname}/${explorePath}`);
+    }
+  };
+
   return (
     <div>
       <Header title={ `Explore ${currentTitle}` } enableSearch={ false } />
-      <ExploreButtons exploreButtons={ foodsButtons } />
+      <ExploreButtons exploreButtons={ renderButtons } handlerClick={ handlerClick } />
       <Footer />
     </div>
   );
