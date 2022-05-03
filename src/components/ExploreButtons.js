878373aa868a60eedb2b695d8091b32d2620fdbe
Davíd Roggér <davidrogger.a@gmail.com>
2022-05-03 09:21:19 -0300

    implementando logica de labelText e testIdLabel

--- src/components/ExploreButtons.js
+++ src/components/ExploreButtons.js
@@ -4,6 +4,17 @@ import PropTypes from 'prop-types';
 import { PATH_FOODS } from '../redux/actions';
 
 function ExploreButtons({ exploreButtons, handlerClick }) {
+  const renderLabel = (text) => {
+    if (text === 'Foods' || text === 'Drinks') return `Explore ${text}`;
+    if (text === 'Surprise') return 'Surprise me!';
+    return `By ${text}`;
+  };
+
+  const testIdLabel = (text) => {
+    if (text === 'surprise me!') return 'explore-surprise';
+    return `explore-${text.replace(' ', '-')}`;
+  };
+
   return (
     <Container className="d-flex flex-column mt-5">
       {exploreButtons.map((button) => {
@@ -12,12 +23,12 @@ function ExploreButtons({ exploreButtons, handlerClick }) {
           ? 'bg-warning text-dark' : 'bg-info text-white';
         return (
           <Button
-            data-testid={ `explore-${lowerText}` }
+            data-testid={ testIdLabel(lowerText) }
             key={ button }
             className={ `m-1 ${dinamicBg}` }
             onClick={ () => handlerClick(lowerText) }
           >
-            { `Explore ${button}` }
+            { renderLabel(button) }
           </Button>
         );
       })}
