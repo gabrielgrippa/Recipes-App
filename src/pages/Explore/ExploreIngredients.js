4a32cab3e6657728b8d868b6ba589eaa0d44fe86
Davíd Roggér <davidrogger.a@gmail.com>
2022-05-04 09:58:03 -0300

    implementado armazenamento dos 12 primeiros ingredientes coletados na API de lista de ingredientes

--- src/pages/Explore/ExploreIngredients.js
+++ src/pages/Explore/ExploreIngredients.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import Loading from '../../components/Loading';
+import routeHelper from '../../services/routeHelper';
+import { searchApi } from '../../services/API';
+import { INGREDIENTS_LIST } from '../../redux/actions';
 
 function ExploreIngredients() {
+  const [ingredients, setIngredients] = useState([]);
+  const { recipeType } = useParams();
+  const route = routeHelper(recipeType);
+
+  const emptyIngredients = ingredients.length === 0;
+
+  const getIngredients = async () => {
+    const ingredientsReq = await searchApi({
+      api: route.currentApiType,
+      searchType: INGREDIENTS_LIST,
+      token: '1',
+    });
+    const ingredientKey = recipeType === 'drinks' ? 'strIngredient1' : 'strIngredient';
+    const QT_MAX = 12;
+    const ingredientsNamesList = ingredientsReq
+      .slice(0, QT_MAX)
+      .map((ingredient) => ingredient[ingredientKey]);
+    setIngredients(ingredientsNamesList);
+  };
+
+  useEffect(() => {
+    getIngredients();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Container>
       <Header title="Explore Ingredients" enableSearch={ false } />
-
+      { emptyIngredients
+        ? <Loading fullPage /> : 'teste'}
       <Footer />
     </Container>
   );
