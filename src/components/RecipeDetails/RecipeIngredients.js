3cf9154cfe5970e2422bcf90eceaf3cf2fbe3c12
Vinícius <hello@marvig.dev>
2022-05-04 15:28:51 -0300

    update recipeingredients component to allow edit

--- src/components/RecipeDetails/RecipeIngredients.js
+++ src/components/RecipeDetails/RecipeIngredients.js
@@ -1,21 +1,64 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Container } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import routeHelper from '../../services/routeHelper';
+import { updateProgress } from '../../redux/actions/profileActions';
 
-function RecipeIngredients({ recipe }) {
+function RecipeIngredients({ recipe, editMode }) {
+  const { api, recipeId } = useParams();
+  const { progressRecipeKey } = routeHelper(api);
+  const dispatch = useDispatch();
+
+  const recipeProgress = useSelector((state) => {
+    const { profileReducer } = state;
+    return profileReducer.inProgressRecipes[progressRecipeKey];
+  })[recipeId] || [];
+
+  const onCheck = ({ target }) => {
+    if (recipeProgress.includes(target.name)) {
+      recipeProgress.splice(recipeProgress.indexOf(target.name), 1);
+    } else {
+      recipeProgress.push(target.name);
+    }
+
+    dispatch(updateProgress(progressRecipeKey, recipeId, recipeProgress));
+  };
+
   return (
     <Container>
       <h5><strong>Ingredients</strong></h5>
 
       { recipe.ingredients.map(({ name, measure }, index) => (
-        <p
-          className="m-0"
-          key={ name }
-          data-testid={ `${index}-ingredient-name-and-measure` }
-        >
-          { `- ${name} ${measure ? `- ${measure}` : ''}` }
-        </p>
-      )) }
+        editMode ? (
+          <div
+            className="form-check"
+            data-testid={ `${index}-ingredient-step` }
+            key={ index }
+          >
+            <input
+              className="form-check-input"
+              type="checkbox"
+              id={ name }
+              name={ index }
+              checked={ recipeProgress.includes(`${index}`) }
+              onChange={ onCheck }
+            />
+            <label className="form-check-label" htmlFor={ name }>
+              { `${name}${measure ? ` - ${measure}` : ''}` }
+            </label>
+          </div>
+        ) : (
+          <p
+            key={ index }
+            className="m-0"
+            data-testid={ `${index}-ingredient-name-and-measure` }
+          >
+            { `${name}${measure ? ` - ${measure}` : ''}` }
+          </p>
+        )))}
+
     </Container>
   );
 }
@@ -23,7 +66,12 @@ function RecipeIngredients({ recipe }) {
 RecipeIngredients.propTypes = {
   recipe: PropTypes.shape({
     ingredients: PropTypes.arrayOf(PropTypes.any),
-  }),
-}.isRequired;
+  }).isRequired,
+  editMode: PropTypes.bool,
+};
+
+RecipeIngredients.defaultProps = {
+  editMode: false,
+};
 
 export default RecipeIngredients;
