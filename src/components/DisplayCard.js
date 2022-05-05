9ec73b4ce4f2a47c78872b337a93987f814f8663
Davíd Roggér <davidrogger.a@gmail.com>
2022-05-05 08:19:58 -0300

    logica condicional para usar displaycard como padrao

--- src/components/DisplayCard.js
+++ src/components/DisplayCard.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card } from 'react-bootstrap';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import { useHistory, useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { exploreIngredients, searchAction } from '../redux/actions/showcaseActions';
+import { INGREDIENT_SEARCH } from '../redux/actions';
 
 function DisplayCard({ recipe, typeCard, index, pathname }) {
   const dinamicBg = pathname === 'foods' ? 'bg-warning text-dark' : 'bg-info text-white';
+  const { recipeType } = useParams();
+  const history = useHistory();
+  const dispatch = useDispatch();
   const redirectPath = typeCard === 'recipe'
     ? `/${pathname}/${recipe.id}` : `/${pathname}`;
+
+  // estou pensando ainda em como refatorar, coloquei muita lógica dentro do display card
+  const handlerClick = (path, ingredient) => {
+    if (typeCard === 'ingredient') {
+      dispatch(exploreIngredients(true));
+      dispatch(searchAction({
+        api: recipeType,
+        searchType: INGREDIENT_SEARCH,
+        token: 1,
+        query: ingredient,
+      }));
+    }
+    history.push(path);
+  };
+
   return (
-    <Link
+    <button
+      type="button"
       data-testid={ `${index}-${typeCard}-card` }
-      to={ redirectPath }
-      style={ { width: '148.5px', margin: '2.5%' } }
+      onClick={ () => handlerClick(redirectPath, recipe.title) }
+      style={
+        { width: '148.5px', margin: '2.5%', border: 'none', backgroundColor: 'white' }
+      }
     >
       <Card className="text-center">
         <Card.Img
@@ -27,7 +51,7 @@ function DisplayCard({ recipe, typeCard, index, pathname }) {
           { recipe.title }
         </Card.Footer>
       </Card>
-    </Link>
+    </button>
   );
 }
 
