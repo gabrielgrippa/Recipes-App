c74b745f0fc612bb9f35f71a6fca3173842f5364
Davíd Roggér <davidrogger.a@gmail.com>
2022-05-06 09:53:10 -0300

    alterando localizacao da ação para requisição dos botoes de filtro mainscreen

--- src/pages/MainScreen.js
+++ src/pages/MainScreen.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams, useHistory } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 import CategoriesButtons from '../components/CategoriesButtons';
 import Header from '../components/Header';
@@ -10,6 +10,7 @@ import SearchBar from '../components/SearchBar';
 import loadRecipes from '../services/loadRecipes';
 import Footer from '../components/Footer';
 import Loading from '../components/Loading';
+import fetchCategories from '../redux/actions/categoriesActions';
 
 function MainScreen() {
   const { recipeType } = useParams();
@@ -17,11 +18,15 @@ function MainScreen() {
   const exploreIngredients = useSelector(
     (state) => state.showcaseReducer.exploreIngredients,
   );
-  console.log(useHistory());
   const dispatch = useDispatch();
   const currentTitle = recipeType[0].toUpperCase() + recipeType.slice(1);
 
-  if (!exploreIngredients) loadRecipes(recipeType, dispatch);
+  useEffect(() => {
+    if (!exploreIngredients) {
+      loadRecipes(recipeType, dispatch);
+      dispatch(fetchCategories());
+    }
+  }, [exploreIngredients, recipeType, dispatch]);
   return (
     <div>
       <Header title={ currentTitle } />
